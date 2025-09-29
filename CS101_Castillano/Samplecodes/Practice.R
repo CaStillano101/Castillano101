9a3c3c4706b413959526db47e750e7c6023fc2a4
CaStilano101 <rashirjohn.castillano@students.isatu.edu.ph>
2025-09-29 07:42:37 +0000

    added files

---
+++ CS101_Castillano/Samplecodes/Practice.R
@@ -0,0 +1,8 @@
+num1 <- 1
+num2 <- 2
+num <- num1 + num2
+num
+git config --global user.email "rashirjohn.castillano@students.isatu.edu.ph"
+git config --global user.name "Your Name"
+git config --global user.email "rashirjohn.castillano@students.isatu.edu.ph"
+git config --global user.name "CaStillano101"
